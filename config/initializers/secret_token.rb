6b333dc5b2bbae882df86b7922d3b03df5e45528
logofhog <thomasscoggins@gmail.com>
2015-05-07 21:17:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FantasyApp::Application.config.secret_key_base = 'b8e269822573484737545dfe4e634bf7f0eb3331af2f5b4724e966d124becbfee27e25d48826cd61a39f9c39344a5813a054de09980b03c5191a510277bc0797'
