052656c73d91f38f152a476570c37dc4eef27b4f
tworker <tworker@onyx.ove.com>
2015-06-27 23:55:49 -0400

    grid work on players page

--- app/assets/javascripts/angular/directives/single_player.js
+++ app/assets/javascripts/angular/directives/single_player.js
@@ -2,6 +2,7 @@ angular.module('fantasy_app')
   .directive('singlePlayer', function(){
     return {
       controller: 'playerCtrl',
+      replace: true,
       templateUrl: 'single_player.html'
     }
   });
