9963132d872a9603dbdbf24b95a1cc02ed6e6920
logofhog <thomasscoggins@gmail.com>
2015-10-21 21:39:31 -0400

    removed all teams toggle names/abbreviation

--- app/assets/javascripts/angular/controllers/teams_ctrl.js
+++ app/assets/javascripts/angular/controllers/teams_ctrl.js
@@ -1,11 +1,7 @@
 angular.module('fantasy_app')
   .controller('teamsCtrl', function($state, $rootScope, teams){
     var vm = this;
-    vm.expandedTeams = (!!$state.params.id) ? false: true
     vm.teams = teams;
 
-    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-      vm.expandedTeams = !$state.params.id
-    });
   });
 
